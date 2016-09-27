289d29b254dd9e1a2443430edeb7b04b0fc5e133
NelsonGomesAntunes <nantunes@ualg.pt>
2016-09-27 17:25:45 -0400

    code - inversion paper Kolaczyk

---
+++ inversion-nodes-degree-Kolaczyk.R
@@ -0,0 +1,23 @@
+library(igraph);
+nv=1000;
+density=100/nv;
+g <- erdos.renyi.game(nv, density);
+
+adj_matrix<-get.adjacency(g);
+#N=degree_distribution(g)*nv;
+nodes_degree=degree(g);
+N=table(nodes_degree);
+p=0.1;
+
+#Ego-centric sampling
+
+Pego=p*diag(nv-1);
+nodes_s_ego <- sample_seq(1, nv, sum(rbinom(nv, 1, p)));
+nodes_degree_sample=nodes_degree[nodes_s_ego];
+N_hat_naive=table(nodes_degree_sample)/p;  
+
+plot(N);
+plot(N_hat_naive);
+
+
+
